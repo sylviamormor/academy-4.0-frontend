0804c87c69c0a9ac1eec84f79f62ac5eb74d16f0
sylviamormor <134374969+sylviamormor@users.noreply.github.com>
2023-10-10 19:29:09 +0000

    new

---
+++ src/components/TimerComponent.vue
@@ -0,0 +1,194 @@
+<script setup>
+import { ref } from 'vue';
+const selectedMinute = ref('');
+const selectedSeconds = ref('');
+
+const selectedTime = ref(null);
+const countries = ref([
+  { name: 'USA' },
+  { name: 'Canada' },
+  { name: 'Australia' },
+  { name: 'United Kingdom' },
+  { name: 'Germany' },
+]);
+</script>
+
+<template>
+    <div class="container">
+        <h3>Timer Setting</h3>
+        <hr />
+        <h4>Set Time</h4>
+
+        <form @submit.prevent="save()">
+          <div class="time">
+            <div class="select">
+              <div class="minute">
+                <select name="minute" id="" v-model="selectedMinute">
+                  <option :value="min" v-for="(min, index) in minutes" :key="index">
+                    {{ min }}
+                  </option>
+                </select>
+              </div>
+              <div class="min">
+                <p>min</p>
+              </div>
+            </div>
+            <div class="select">
+              <div class="seconds">
+                <select name="seconds" id="" v-model="selectedSeconds">
+                  <option :value="sec" v-for="(sec, index) in seconds" :key="index">
+                    {{ sec }}
+                  </option>
+                </select>
+              </div>
+              <div class="sec">
+                <p>sec</p>
+              </div>
+            </div>
+          </div>
+          <button type="submit">Save</button>
+        </form>
+
+        <div>
+            <label class="datalist-table" for="countries"></label>
+        <input list="countryList" type="text" id="countries" placeholder="00" name="countries" v-model="selectedTime" />
+
+        <datalist id="countryList">
+          <option v-for="country in countries" :value="country" :key="country"></option>
+        </datalist>
+        </div>
+    </div>
+</template>
+
+
+
+
+<style scoped>
+.datalist-table{
+    /* border: 10px black solid; */
+    padding: 10px;
+}
+#countries{
+    display: none;
+}
+.timing {
+    /* width: 50px; */
+    border: none;
+    /* font-size: 100px; */
+}
+
+.datalist {
+    display: none;
+}
+
+.setting-div {
+    display: flex;
+    gap: 100px;
+}
+
+.time {
+    display: flex;
+    gap: 0 40px;
+    margin-top: 10px;
+    margin-bottom: 34px;
+}
+
+img {
+    padding: 10px 40px 0px 10px;
+}
+
+button {
+    background: #7557d3;
+    border-radius: 3px;
+    height: 38px;
+    width: 127px;
+    border-radius: 3px;
+    color: white;
+    border: none;
+    cursor: pointer;
+}
+
+h3 {
+    font-style: normal;
+    font-weight: 700;
+    font-size: 16px;
+    line-height: 19px;
+    letter-spacing: -0.4px;
+    color: #4a4a4a;
+}
+
+h4 {
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 17px;
+    color: #2b3c4e;
+}
+
+h5 {
+    font-style: normal;
+    font-weight: 300;
+    font-size: 48px;
+    line-height: 58px;
+    color: #2b3c4e;
+}
+
+hr {
+    width: 724px;
+    height: 1px;
+    left: 340px;
+    top: 432px;
+    background: #ffffff;
+    border: 1px solid #f2f2f2;
+    margin-top: 16px;
+    margin-bottom: 100px;
+}
+
+.minute,
+.seconds {
+    width: 100px;
+    font-family: "Lato";
+    display: grid;
+    grid-template-areas: "minute";
+    align-items: center;
+}
+
+
+option {
+    color: #000;
+    font-size: 16px;
+}
+
+.minute::after,
+.seconds::after {
+    content: url("@/assets/entries-logo.svg");
+    justify-self: end;
+}
+
+select,
+.minute:after,
+.seconds:after {
+    grid-area: select;
+}
+
+p {
+    font-size: 12px;
+}
+
+.select {
+    position: relative;
+    display: flex;
+    align-items: center;
+}
+
+.min,
+.sec {
+    position: absolute;
+    bottom: 0;
+    left: 50px;
+}
+
+.sec {
+    position: absolute;
+    left: 75px;
+}</style>
---
+++ src/views/admin/ApplicationEntriesView.vue
@@ -0,0 +1,203 @@
+<script setup>
+import {ref, computed, onMounted} from 'vue';
+import ModalComponent from '../../components/ModalComponent.vue';
+import DashboardTitleComponent from '../../components/DashboardTitleComponent.vue';
+import axios from "axios";
+
+const openmodal = ref(false);
+
+const openMainModal = () => {
+    openmodal.value = true;
+};
+
+const openDenyButton = () => {
+    openmodal.value = false;
+    declineVisibility.value = true;
+};
+
+const openApproveButton = () => {
+    openmodal.value = false;
+    approveVisibility.value = true;
+};
+
+const closeModal = () => {
+    openmodal.value = false;
+};
+
+// const people = ref([
+//     { id: 1, name: 'jack jack', age: 12, email: 'jack@mail.com', address: '21 adom str.', university: 'University of Lagos', cgpa: 2.54 },
+//     { id: 2, name: 'Bongo', age: 13, email: 'bongo@mail.com', address: '21 adom str.', university: 'University of Lagos', cgpa: 3.54 },
+//     { id: 3, name: 'congo', age: 14, email: 'congo@mail.com', address: '21 adom str.', university: 'University of Lagos', cgpa: 3.24 },
+// ]);
+
+const sortedPeople = computed(() => {
+    return [...people.value].sort((a, b) => a.age - b.age || a.cgpa - b.cgpa);
+});
+
+const ageAscending = () => {
+    people.value.sort((a, b) => b.age - a.age);
+};
+const ageDescending = () => {
+    people.value.sort((a, b) => b.age - a.age);
+};
+
+const cgpaAscending = () => {
+    people.value.sort((a, b) => b.cgpa - a.cgpa);
+};
+const cgpaDescending = () => {
+    people.value.sort((a, b) => b.cgpa - a.cgpa);
+};
+
+const people = ref([])
+
+/*
+* methods
+ */
+
+function setPeople(data) {
+  people.value = data
+}
+
+async function getAllApplicants() {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get("//localhost:7006/api/v1/application", {
+      headers: { authorization: token },
+    });
+    setPeople(response.data.data)
+    console.log(response)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+/*
+* computed
+ */
+
+const fullName = computed(() => {
+  return (firstName, lastName) => {
+    return `${firstName} ${lastName}`
+  }
+});
+
+onMounted(async () => {
+  await getAllApplicants()
+})
+
+
+</script>
+
+<template>
+ <div class="container">
+  <ModalComponent @close="closeModal" v-show="openmodal" class="main-modal" />
+  
+  <DashboardTitleComponent 
+      cardTitle="Entries - Batch 1"
+      cardText="Comprises of all that applied for batch 1"
+  />
+      
+        <table style="width: 100%">
+            <thead >
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th class="sorting">
+                        DOB - Age
+                        <div class="icons">
+                            <button class="btn" @click="ageAscending"><img src="../../assets/icons/sortup.svg" alt="sortup" srcset=""></button>
+                            <button class="btn" @click="ageDescending"><img src="../../assets/icons/sortdown.svg" alt="sortdown" srcset=""></button>
+                        </div>
+                    </th>
+                    <th>Address</th>
+                    <th>University</th>
+                    <th class="sorting">
+                        CGPA
+                        <div class="icons">
+                            <button class="btn" @click="cgpaAscending"><img src="../../assets/icons/sortup.svg" alt="sortup" srcset=""></button>
+                            <button class="btn" @click="cgpaDescending"><img src="../../assets/icons/sortdown.svg" alt="sortdown" srcset=""></button>
+                        </div>
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="t-row" v-for="person in sortedPeople" :key="person.id" @click="openMainModal">
+                    <td>{{ fullName(person.first_name, person.last_name) }}</td>
+                    <td>{{ person.email }}</td>
+                    <td>{{ person.date_of_birth }}</td>
+                    <td>{{ person.address }}</td>
+                    <td>{{ person.university }}</td>
+                    <td>{{ person.cgpa }}</td>
+                </tr>
+            </tbody>
+        </table>
+      </div>    
+
+</template>
+
+
+<style scoped>
+.container{
+    height: 100vh;
+    padding: 60px 47px;
+    overflow-y: scroll;
+}
+
+table {
+  border-collapse: collapse;
+   width:100%;
+   margin-top: 38px;
+}
+
+thead{
+    background-color: #2B3C4E;
+    color: #fff;
+}
+th{
+    color: #fff;
+    padding: 14px 10px;
+}
+.sorting{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.icons{
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+}
+img {
+  cursor: pointer;
+}
+tbody {
+    border-radius: 8px 0px 0px 8px;
+}
+.t-row:hover {
+    margin-top: 20px;
+    background: #ffffff;
+  box-shadow: 0px 5px 15px rgba(33, 31, 38, 0.05);
+  border-left: 7px solid #7557d3;
+  cursor: pointer;
+}
+
+td{
+    padding: 22px 10px;
+    text-align: center;
+    border-radius: 8px;
+}
+.btn{
+    border: none;
+    background-color: transparent;
+    padding: 0;
+    margin: 0;
+    border: #7557D3 1px solid;
+}
+img {
+}
+
+.main-modal {
+  position: absolute;
+}
+
+</style>
